b977b906fe6a9d0004e96ea107a99e69f0362283
MoedCode <mohamed71291@gmail.com>
2024-03-04 06:16:39 +0200

    Real Work is started but with dummy data 😎

--- StoreMain.py
+++ StoreMain.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 from flask import Flask,  render_template
-
+from data0 import *
 APP = Flask(__name__)
 HOST, PORT = '0.0.0.0' , 5005
 APP.url_map.sort_key = False
 APP.debug = True
-pamps_list = ["Product0","Product1","Product2","Product3"]
+# Updated items list
+
 
 @APP.route("/")
 @APP.route("/home")
 def routeHome():
 
-    return render_template("1.html", pamps_list = pamps_list, ROUT="/" )
+    return render_template("home.html")
+@APP.route('/market')
+# @APP.route('/prostore/market')
+def rout_market():
+    return render_template('market.html', products_list=products_list)
 @APP.route("/user/<username>")
 def rout_usr(username):
     pass
+
 
 
 if __name__ == "__main__":
