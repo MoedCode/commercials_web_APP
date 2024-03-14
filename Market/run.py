02be2499bad85d6e7bc557d15e2e4493b623875e
MoedCode <mohamed71291@gmail.com>
2024-03-14 23:54:11 +0200

    Thu Mar 14 23:54:11 EET 2024

--- Market/run.py
+++ Market/run.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
+import sys
+import os
+
+# Get the directory of the current script
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Append the parent directory to the system path
+parent_directory = os.path.join(script_directory, '..')
+sys.path.append(parent_directory)
+
 from Market import app
 
 if __name__ == "__main__":
