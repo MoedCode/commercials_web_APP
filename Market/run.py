c1e5f2ec82b23c902b373880acae7e1d1f5b5c92
MoedCode <mohamed71291@gmail.com>
2024-03-15 03:59:32 +0200

    Fri Mar 15 03:59:32 EET 2024

--- Market/run.py
+++ Market/run.py
@@ -12,7 +12,7 @@ sys.path.append(parent_directory)
 from Market import app
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5005, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 
 
